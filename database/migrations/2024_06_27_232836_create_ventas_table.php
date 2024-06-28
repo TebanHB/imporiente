21c4918f33ace4848b2129e183e846355585ff09
Esteban Hurtado <TebanHB>
2024-06-28 00:29:54 -0400

    Modal cotizacion

---
+++ database/migrations/2024_06_27_232836_create_ventas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('fecha_venta')->nullable();
+            $table->date('expiracion_oferta')->nullable();
+            $table->foreignId('cliente_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('vendedor_id')->constrained('users')->onDelete('cascade');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
--- resources/views/admin/cotizaciones/create.blade.php
+++ resources/views/admin/cotizaciones/create.blade.php
@@ -7,6 +7,37 @@
     </h1>
 @stop
 @section('content')
+<!-- Modal -->
+<div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="cartModalLabel"><b> Agregar al Carrito:</b> <span id="productName" class="float-right"></span></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="addToCartForm">
+                    <div class="form-group">
+                        <label>Precio sugerido: <span id="suggestedPrice">Precio</span></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="salePrice">Precio de venta:</label>
+                        <input type="number" class="form-control" id="salePrice" name="salePrice" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantity">Cantidad:</label>
+                        <input type="number" class="form-control" id="quantity" name="quantity" required>
+                    </div>
+                    <input type="hidden" id="productId" name="productId">
+                    <button type="submit" class="btn btn-primary">Agregar al carrito</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="row">
     @foreach($productos as $producto)
             <div class="col-md-4 col-sm-6 col-xs-12">
@@ -16,7 +47,7 @@
                         <h5 class="card-title">{{ $producto->nombre }}</h5>
                         <p class="card-text">{{ Str::limit($producto->descripcion, 100) }}</p>
                         <a href="{{ route('admin.productos.show', $producto->id) }}" class="btn btn-primary">Ver más</a>
-                        <a href="#" class="btn btn-success">Agregar al carrito</a>
+                        <a href="#" class="btn btn-success addToCartBtn" data-id="{{ $producto->id }}" data-price="{{ $producto->precio }}" data-name="{{ $producto->nombre }}">Agregar al carrito</a>
                     </div>
                 </div>
             </div>
@@ -36,7 +67,39 @@
 @stop
 
 @section('js')
-    <script></script>
+    <script>
+    $(document).ready(function() {
+        $('.addToCartBtn').click(function(e) {
+            e.preventDefault();
+            var productId = $(this).data('id');
+            var productPrice = $(this).data('price');
+            var productName = $(this).data('name'); // Asume que el botón tiene un atributo data-name
+            $('#productId').val(productId);
+            $('#suggestedPrice').text(productPrice);
+            $('#productName').text(productName); // Actualiza el nombre del producto en el modal
+            $('#cartModal').modal('show');
+        });
+    
+        $('#addToCartForm').submit(function(e) {
+            e.preventDefault();
+            var formData = $(this).serialize(); // Captura los datos del formulario
+            $.ajax({
+                type: "POST",
+                url: "/ruta/a/tu/controlador", // Cambia esto por la ruta a tu controlador
+                data: formData,
+                success: function(response) {
+                    $('#cartModal').modal('hide');
+                    // Aquí puedes agregar una notificación de éxito
+                    alert("Producto agregado al carrito");
+                },
+                error: function(error) {
+                    // Manejo de errores
+                    alert("Error al agregar el producto al carrito");
+                }
+            });
+        });
+    });
+    </script>
     <script>
         console.log("Hi, I'm using the Laravel-AdminLTE package!");
     </script>
